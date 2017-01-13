ea2344827a033dd142f85d0d9e3e3dd7775e6c2e
Vivien-lb <vivien-lb@hotmail.fr>
2017-01-13 11:57:14 +0100

    Checkpoints, press 'K' pour simuler une mort.

---
+++ Assets/Scripts/CheckPoints.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckPoints : MonoBehaviour {
+
+	public GameObject player;
+	public int checkNumber;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnTriggerEnter(Collider col) {
+		print ("yolo");
+		int lastCheck = player.GetComponent<Death> ().checkNumber;
+		if (lastCheck < checkNumber) {
+			
+			player.GetComponent<Death> ().checkNumber = checkNumber;
+			player.GetComponent<Death> ().checkpoint = transform.position;
+		}
+	}
+}
---
+++ Assets/Scripts/Death.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Death : MonoBehaviour {
+
+	public GameObject player;
+	public int checkNumber;
+	public Vector3 checkpoint;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+		checkNumber = 0;
+		checkpoint = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.K)) {
+
+			player.GetComponent<TransforSpaceship> ().acceleration = 0F	;
+			transform.rotation = Quaternion.Euler (0, 0, 0);
+			transform.position = checkpoint;
+		}
+	}	
+}
