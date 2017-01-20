ba35bfb845378c09fdaca2a80807089e8815ea23
tparadis <theophile.paradis@gmail.com>
2017-01-20 12:01:56 +0100

    nouvelle physique deplacement + change camera

---
+++ Assets/Scripts/ChangeCamera.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeCamera : MonoBehaviour {
+
+	public Camera cameraFirstPerson;
+	public Camera cameraSemiPerson;
+	public Camera cameraThirdPerson;
+	public Camera cameraHelico;
+
+	public int cameraNum;
+
+	void Start()
+	{
+		cameraFirstPerson.enabled = true;
+		cameraThirdPerson.enabled = false;
+		cameraSemiPerson.enabled = false;
+		cameraHelico.enabled = false;
+		cameraNum = 0;
+
+	}
+	void Update () 
+	{
+		if (Input.GetKeyDown(KeyCode.A))
+		{
+			switch (cameraNum) {
+			case 0: 
+				cameraFirstPerson.enabled = false;
+				cameraSemiPerson.enabled = true;
+
+				cameraNum =(cameraNum + 1) % 4;
+				break;
+			case 1: 
+				cameraThirdPerson.enabled = true;
+				cameraSemiPerson.enabled = false;
+				cameraNum =(cameraNum + 1)  % 4;
+				break;
+			case 2: 
+				cameraThirdPerson.enabled = false;
+				cameraHelico.enabled = true;
+				cameraNum =(cameraNum + 1 ) % 4;
+				break;
+			case 3: 
+				cameraFirstPerson.enabled = true;
+				cameraHelico.enabled = false;
+				cameraNum =(cameraNum + 1)  % 4;
+				break;
+			}
+		}
+	}
+}
--- Assets/Scripts/TransforSpaceship.cs
+++ Assets/Scripts/TransforSpaceship.cs
@@ -4,56 +4,82 @@ using System.Collections;
 public class TransforSpaceship : MonoBehaviour {
 
 	public float acceleration = 0F;
-	public float speedLimit= 30.0F;
+	public float speedLimit= 15.0F;
 	public float boostLimit= 50.0F;
-	public float turnSpeed = 50f;
+	public float turnAccel = 5F;
+	public float turnSpeed = 100f;
+	private Vector3 direction;
+	public Rigidbody rb;
 
 
 	private Vector3 moveDirection = Vector3.zero;
 	// Use this for initialization
 	void Start () {
+		rb = GetComponent<Rigidbody>();
 	}
 
 	// Update is called once per frame
 
 	void Update() {
 
+	}
+	void FixedUpdate(){
 
-		if(Input.GetKey(KeyCode.LeftArrow))
-			transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
-
-		if(Input.GetKey(KeyCode.RightArrow))
-			transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
-
-		if (Input.GetKey (KeyCode.Tab)) {
-			transform.Translate (Vector3.forward* acceleration* Time.deltaTime);
-			if (acceleration < (speedLimit*1.2)) {
-				this.acceleration *= 1.2F;	
-			}
-
-
-		}
-		if (Input.GetKey (KeyCode.UpArrow)) {
-			if (acceleration < 1F) {
-				acceleration = 1F;
-			}
-			transform.Translate (Vector3.forward * acceleration * Time.deltaTime);
-			if (acceleration < speedLimit) {
-				this.acceleration *= 1.03F;	
-			}
-		} else {
-			transform.Translate(Vector3.forward * acceleration * Time.deltaTime);
-
-		}
+		//perte de vitesse
 		if (acceleration > 1) {
 			this.acceleration *= 0.99F;
 		}
-
-		if (Input.GetKey(KeyCode.Space))
-		{
+		//brake
+		if (Input.GetKey (KeyCode.Space)) {
 			this.acceleration *= 0.97F;			
 		}
 
-	}
+		if (Input.GetKey (KeyCode.Tab)) {
+			rb.AddRelativeForce (new Vector3 (0, 0, acceleration));
+			if (acceleration < (speedLimit * 1.2)) {
+				this.acceleration *= 1.2F;	
+			}
+		}
 
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+
+			transform.RotateAroundLocal(Vector3.up, -turnAccel* Time.deltaTime);
+			rb.AddRelativeForce (new Vector3 (0, 0, acceleration));
+
+		} else {
+			//TurnRight
+			if (Input.GetKey (KeyCode.RightArrow)) {
+
+				transform.RotateAroundLocal(Vector3.up,  turnAccel* Time.deltaTime);
+				rb.AddRelativeForce (new Vector3 (0, 0, acceleration));
+
+
+			}
+
+			if (Input.GetKey (KeyCode.DownArrow)) {
+				if (acceleration < 5F) {
+					acceleration = 10F;
+				}
+				direction = Vector3.back;
+				rb.AddRelativeForce (new Vector3 (0, 0, -acceleration));
+
+				if (acceleration < speedLimit) {
+					this.acceleration *= 1.03F;	
+				}
+			} else {
+				//forward
+				if (Input.GetKey (KeyCode.UpArrow)) {
+					if (acceleration < 5F) {
+						acceleration = 10F;
+					}
+					direction = Vector3.forward;
+					rb.AddRelativeForce (new Vector3 (0, 0, acceleration));
+
+					if (acceleration < speedLimit) {
+						this.acceleration *= 1.03F;	
+					}
+				}
+			}
+		}
+	}
 }
