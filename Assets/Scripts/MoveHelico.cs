8cd696a227a0ac90451327fda9cefcbfaaee538f
Kellam <Florianguenec@hotmail.fr>
2017-03-05 16:15:25 +0100

    han solo 2.0

--- Assets/Scripts/MoveHelico.cs
+++ Assets/Scripts/MoveHelico.cs
@@ -6,27 +6,15 @@ public class MoveHelico : MonoBehaviour {
 	public GameObject vaisseau;
 	private Vector3 target;
 	private float step;
-	private float previousZ;
 	// Use this for initialization
 	void Start () {
 		step = 100f * Time.deltaTime;
-		previousZ = vaisseau.transform.position.z;
 		}
 	
 	// Update is called once per frame
 	void Update () {
 		target = new Vector3(vaisseau.transform.position.x,transform.position.y,vaisseau.transform.position.z);
-		/*if (previousZ < vaisseau.transform.position.z - 2f || previousZ > vaisseau.transform.position.z + 2f) {
-			var rotation = Quaternion.LookRotation (target);
-			transform.rotation = Quaternion.Slerp (transform.rotation, rotation, Time.deltaTime);
-			//transform.LookAt (target);
-			previousZ = vaisseau.transform.position.z;
-		} else {
-		*/	transform.position = Vector3.MoveTowards (transform.position, target, step);
-		//}
-		//print (vaisseau.transform.position.z);
-		//print ("previous Z = " + transform.position.z);
-		
+		transform.position = Vector3.MoveTowards (transform.position, target, step);
 	} 
 
 }
